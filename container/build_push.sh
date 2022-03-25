b4bb9ae73c48b28abeff165d61b907d59289efe5
Sergio Méndez <sergio.mendez@yalo.com>
2022-03-25 11:01:12 -0600

    feat: update for PMI demo

--- container/build_push.sh
+++ container/build_push.sh
@@ -1,2 +1,5 @@
 docker login
-docker buildx build --platform linux/arm64 -t $1/myapp --push .
+#This line is for ARM
+#docker buildx build --platform linux/arm64 -t $1/myapp --push .
+docker build -t $1/myapp .
+docker push $1/myapp
